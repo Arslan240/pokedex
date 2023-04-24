82e4f55ee146e059514c929145513850e9d9278f
Arslan240 <arslan.jamil1322@gmail.com>
2023-04-24 16:57:41 +0500

    Wrote the species section of about page in a cleaner way

--- src/Routes/Pokemon/About.jsx
+++ src/Routes/Pokemon/About.jsx
@@ -74,43 +74,43 @@ const About = () => {
   const genderURL = `https://pokeapi.co/api/v2/gender/${id}/`
   const eggGroupsURL = `https://pokeapi.co/api/v2/pokemon-species/${id}/`
   const eggCyclesURL = `https://pokeapi.co/api/v2/pokemon-species/${id}/`
-  
-  
+
+
   useEffect(() => {
-    
-    async function fetchData(){
-      const {height, weight, abilities} = await fetchHeightAndWeight()
+
+    async function fetchData() {
+      const { height, weight, abilities } = await fetchHeightAndWeight()
       const speciesName = await fetchSpecies()
-      
+
       const genderName = await fetchPokemonGender()
       // const responseGender = await fetch(genderURL)
       // const {name: genderName} = await responseGender.json()
-      
-      const {eggGroups, hatch_counter} = await fetchEggGroupsAndEggCycle()
-      
-      setSpeciesDetails([{speciesName},{height}, {weight},  {abilities}])
-      setBreedingDetails([{genderName}, {eggGroups}, {hatch_counter}])
+
+      const { eggGroups, hatch_counter } = await fetchEggGroupsAndEggCycle()
+
+      setSpeciesDetails([{ speciesName }, { height }, { weight }, { abilities }])
+      setBreedingDetails([{ genderName }, { eggGroups }, { hatch_counter }])
     }
 
-    async function fetchHeightAndWeight(){
+    async function fetchHeightAndWeight() {
       try {
         const responsePokeDetails = await fetch(pokeDetailsURL)
         let { abilities = [], height, weight } = await responsePokeDetails.json()
         const abts = abilities.map(({ ability }) => ability.name)
         height = height / 10; // decimeter to meter conversion.
         weight = weight / 10; // hectogram to kilogram conversion.
-        return {height, weight, abilities: abts.join(', ')}
+        return { height, weight, abilities: abts.join(', ') }
       } catch (error) {
         console.log("Error in height, weight or abilities")
         console.log(error.message)
       }
     }
 
-    async function fetchSpecies(){
+    async function fetchSpecies() {
       try {
         const responseSpecies = await fetch(speciesURL)
         const speciesData = await responseSpecies.json()
-        const {name: speciesName} = speciesData.chain.species
+        const { name: speciesName } = speciesData.chain.species
         return speciesName
       } catch (error) {
         console.log("Error in fetching species name")
@@ -118,7 +118,7 @@ const About = () => {
       }
     }
 
-    async function fetchPokemonGender(){
+    async function fetchPokemonGender() {
       try {
         const response = await fetch(eggGroupsURL);
         const data = await response.json();
@@ -131,18 +131,18 @@ const About = () => {
       }
     }
 
-    function determineGender(pokemonSpecies){
+    function determineGender(pokemonSpecies) {
       const physicalAttackIV = Math.floor(Math.random() * 16);
       const genderRate = pokemonSpecies.gender_rate;
 
       if (genderRate === -1) {
         return 'Gender unknown';
       }
-    
+
       if (genderRate === 0) {
         return 'Always male';
       }
-    
+
       if (genderRate === 8) {
         return 'Always female';
       }
@@ -153,24 +153,24 @@ const About = () => {
       } else {
         return 'Male';
       }
-    
+
     }
 
-    async function fetchEggGroupsAndEggCycle(){
+    async function fetchEggGroupsAndEggCycle() {
       try {
         const responseEggGroup = await fetch(eggGroupsURL)
-        const {egg_groups: eggGroupDetails, hatch_counter} = await responseEggGroup.json()
+        const { egg_groups: eggGroupDetails, hatch_counter } = await responseEggGroup.json()
         const eggGroups = eggGroupDetails?.map(detail => {
           return detail.name;
         });
-        return {eggGroups: eggGroups.join(', '), hatch_counter}  
+        return { eggGroups: eggGroups.join(', '), hatch_counter }
       } catch (error) {
         console.log(error.message)
       }
     }
     fetchData()
-  },[id])
-  
+  }, [id])
+
   console.log(speciesDetails)
   console.log(breedingDetails)
   console.log(id, "in About")
@@ -185,21 +185,24 @@ const About = () => {
             <span>abilities</span>
           </div>
           <div className="flex-col">
-          {speciesDetails.length > 0 &&
+            {speciesDetails.length > 0 &&
+            
               speciesDetails.map((detail) => {
-                const [key, value] = Object.entries(detail)
+                const [key, value] = Object.entries(detail)[0];
 
-                console.log(key[0], key[1])
-                
-                if(key[0] === 'height'){
-                  return <span className='no-cap'>{key[1]} m</span> 
+                if (key === "height" || key === "weight") {
+                  return (
+                    <span className='no-cap' key={key}>
+                      {value} {key === "height" ? "m" : "kg"}
+                    </span>
+                  );
                 }
-                else if(key[0] === 'weight'){
-                  return <span className='no-cap'>{key[1]} kg</span>
-                }
-                else{
-                  return <span>{key[1]}</span>
-                }
+
+                return (
+                  <span key={key}>
+                    {value}
+                  </span>
+                );
               })
             }
           </div>
@@ -214,27 +217,27 @@ const About = () => {
             <span>egg cycle</span>
           </div>
           <div className="flex-col">
-          {breedingDetails.length > 0 &&
-              
+            {breedingDetails.length > 0 &&
+
               breedingDetails.map((detail) => {
                 const regexMale = /\bmale\b/i;
                 const regexFemale = /\bfemale\b/i;
                 const [key, value] = Object.entries(detail)[0];
-              
+
                 if (key === "genderName") {
                   const icon = value.toLowerCase().match(regexMale)
-                    ? <GiMale color='#223eda'/>
+                    ? <GiMale color='#223eda' />
                     : value.toLowerCase().match(regexFemale)
-                    ? <GiFemale color='#de3434'/>
-                    : <FaGenderless color='#ffd437'/>;
-              
+                      ? <GiFemale color='#de3434' />
+                      : <FaGenderless color='#ffd437' />;
+
                   return (
                     <span key={key}>
                       {icon} {value}
                     </span>
                   );
                 }
-              
+
                 return (
                   <span key={key}>
                     {value}
